e2f03c3bad0064cd94c0a61c4175d524ddd92594
Harry <henry.iz@yandex.ru>
2023-10-27 19:51:20 +0300

    Add color property

--- python/classes_02.py
+++ python/classes_02.py
@@ -1,6 +1,7 @@
 class Berenjena:
     def __init__(self):
         self.__name = 'berenjena'
+        self.__color = 'purple'
 
     def __str__(self) -> str:
         return self.__name
@@ -12,8 +13,14 @@ class Berenjena:
     @name.setter
     def name(self, name):
         self.__name = name
+
+    @property
+    def color(self):
+        return self.__color
+
     
 berenjena = Berenjena()
 print(berenjena.name)
 berenjena.name = 'naranja + berenjena'
 print(berenjena.name)
+print(self.color)
