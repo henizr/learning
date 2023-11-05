e36d683d7a5fbce68746c77d3185e05c79caecc9
Harry <henry.iz@yandex.ru>
2023-11-05 20:34:36 +0300

    Modified python folder

--- python/tarber_01.py
+++ python/tarber_01.py
@@ -1,1 +1,30 @@
-print([1,2,32,2,1,1].count(1))
+# print([1,2,32,2,1,1].count(1))
+# print(list(map(  lambda a: a*11, [1,2,4,10])))
+
+# cups = int(input(">"))
+# print(cups//6)
+
+# turkey = int(input(">")) * 2
+# pigs = int(input(">")) * 4
+# cows = int(input(">")) * 4
+
+# print(turkey + pigs  + cows)
+
+from collections import Counter
+
+
+
+nums = [11,1,1,1]
+nums2 = [5,5,211,5]
+count1 =  Counter(nums)
+count2 =  Counter(nums2)
+print(count1 - count2)
+
+import itertools
+
+# for i in itertools.cycle([0,1]):
+#     print(i)
+
+
+for result in itertools.accumulate([0,1, 2,3,4,5]): '''iterating'''
+print(result)
---
+++ python/tarber_02.py
@@ -0,0 +1,27 @@
+import itertools
+import time
+from collections import defaultdict
+
+# for item in itertools.cycle(range(10000)):
+#     print(item)
+#     time.sleep(0.5)
+
+
+
+# for result in itertools.accumulate(
+#     [10, 100, 101, -1, 1],
+#     lambda a, b: a*b):
+#     """accumulation"""
+
+# print(result)
+
+
+# def_dict = defaultdict(lambda : 'default value')
+# def_dict["id"] = 1
+
+# print(def_dict["id"])
+# print(def_dict["name"])
+
+
+
+
