38419e82e8323ed62fdbd8da42e6a15a430bb43e
Harry <henry.iz@yandex.ru>
2023-11-23 16:50:46 +0300

    Modified java folder

---
+++ _java/WeatherStation_02/WeatherData.java
@@ -0,0 +1,19 @@
+package learning._java.WeatherStation_02;
+
+public class WeatherData implements Subject{
+    
+    @Override
+    public void registerObserver(){
+
+    }
+
+    @Override
+    public void removeObserver(){
+
+    }
+
+    @Override
+    public void notifyObserver(){
+        
+    }
+}
