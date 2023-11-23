45f3a386b25ccc3f68cb3d022af5acfb107350c7
Harry <henry.iz@yandex.ru>
2023-11-23 20:50:32 +0300

    Modified metronom project

---
+++ metronom/script.js
@@ -0,0 +1,6 @@
+const circle = document.querySelector(".circle");
+const button = document.querySelector("button");
+
+button.addEventListener('click', function(event){
+    console.log(circle);
+});
