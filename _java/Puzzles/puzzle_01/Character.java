47203c7d2092f9da942df07905e799a835df2ad3
Harry <henry.iz@yandex.ru>
2023-11-27 18:45:25 +0300

    Added the pazzle project

---
+++ _java/Puzzles/puzzle_01/Character.java
@@ -0,0 +1,13 @@
+package learning._java.Puzzles.puzzle_01;
+
+/**
+ * Character
+ */
+public class Character {
+
+    WeaponBehavior weapon;
+
+    public void fight(){
+        weapon.useWeapon();
+    }
+}
---
+++ _java/Puzzles/puzzle_01/Program.java
@@ -0,0 +1,8 @@
+package learning._java.Puzzles.puzzle_01;
+
+public class Program {
+    public static void main(String[] args) {
+        King king = new King();
+        king.fight();
+    }
+}
